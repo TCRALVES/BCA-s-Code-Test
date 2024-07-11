50de93da74918ae999b1325683d4f5227937f4f3
Thiago Alves <thiago.al.engineering@fakedoor.com>
2024-07-11 05:33:20 +0100

    dll's

--- tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
+++ tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc6940365b4510c91981af7b1cc6e9c1eca9bbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f276b83d324d41014e99576c96f68e48ef9a9cfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
